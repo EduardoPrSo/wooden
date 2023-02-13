6f4bb1bc4238bdca541abd882570f40088cf0962
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-02-13 08:42:51 -0300

    Update ProductsDisplay.jsx

--- src/components/ProductsDisplay/ProductsDisplay.jsx
+++ src/components/ProductsDisplay/ProductsDisplay.jsx
@@ -5,10 +5,12 @@ import { useEffect, useState } from 'react';
 export default function ProductsDisplay({ productsData }) {
     const router = useRouter();
     const [windowWidth, setWindowWidth] = useState(0);
+    const [windowHeight, setWindowHeight] = useState(0);
     const [scrollY, setScrollY] = useState(0);
 
     useEffect(() => {
-        setWindowWidth(window.innerWidth)
+        setWindowWidth(window.innerWidth);
+        setWindowHeight(window.innerHeight);
     }, [])
 
     useEffect(() => {
@@ -29,12 +31,12 @@ export default function ProductsDisplay({ productsData }) {
         <div key={index} className={styles.productDiv}>
             {windowWidth > 900 ? index % 2 === 0 ? (
                 <>
-                    <div className={styles.productImage} style={{transform: scrollY > (500+(index*(windowWidth/5))) ? "translateX(0)" : "translateX(-200%)", transition: '1s' }}>
+                    <div className={styles.productImage} style={{transform: scrollY > (500+(index*(windowHeight/2))) ? "translateX(0)" : "translateX(-200%)", transition: '1s' }}>
                         <div className={styles.imageCarousel}>
                             <img src={product.images[0]} style={{ width: '25vw', height: '70vh', boxShadow: '5px 5px 10px #808080'}} />
                         </div>
                     </div>
-                    <div className={styles.productInfo} style={{width: '30vw', height: '55vh', marginTop: '10%', transform: scrollY > (500+(index*(windowWidth/5))) ? "translateX(0)" : "translateX(+200%)", transition: '1s'  }}>
+                    <div className={styles.productInfo} style={{width: '30vw', height: '55vh', marginTop: '10%', transform: scrollY > (500+(index*(windowHeight/2))) ? "translateX(0)" : "translateX(+200%)", transition: '1s'  }}>
                         <h1 style={{marginBottom: '5%'}}>{product.title}</h1>
                         <p style={{marginBottom: '5%', textAlign: 'justify'}}>{product.description}</p>
                         <button className={styles.productButton} onClick={()=>productRedirect(product._id)}>Saiba mais</button>
@@ -42,14 +44,14 @@ export default function ProductsDisplay({ productsData }) {
                 </>
             ) : (
                 <>
-                    <div className={styles.productInfo} style={{width: '30vw', height: '55vh', marginTop: '10%', transform: scrollY > (500+(index*(windowWidth/5))) ? "translateX(0)" : "translateX(-200%)", transition: '1s'  }}>
+                    <div className={styles.productInfo} style={{width: '30vw', height: '55vh', marginTop: '10%', transform: scrollY > (500+(index*(windowHeight/2))) ? "translateX(0)" : "translateX(-200%)", transition: '1s'  }}>
                         <h1 style={{textAlign: 'right', marginBottom: '5%'}}>{product.title}</h1>
                         <p style={{textAlign: 'right', marginBottom: '5%', textAlign: 'justify'}}>{product.description}</p>
                         <div style={{width: '100%', display: 'flex', justifyContent: 'right'}}>
                             <button className={styles.productButton} onClick={()=>productRedirect(product._id)}>Saiba mais</button>
                         </div>
                     </div>
-                    <div className={styles.productImage} style={{transform: scrollY > (500+(index*(windowWidth/5))) ? "translateX(0)" : "translateX(+200%)", transition: '1s' }}>
+                    <div className={styles.productImage} style={{transform: scrollY > (500+(index*(windowHeight/2))) ? "translateX(0)" : "translateX(+200%)", transition: '1s' }}>
                         <div className={styles.imageCarousel}>
                             <img src={product.images[0]} style={{ width: '25vw', height: '70vh', boxShadow: '5px 5px 10px #808080' }} />
                         </div>
