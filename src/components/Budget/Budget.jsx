21ea670460e3c19388e88878ddc0d206e03e1919
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-02-14 17:23:31 -0300

    update

--- src/components/Budget/Budget.jsx
+++ src/components/Budget/Budget.jsx
@@ -4,14 +4,12 @@ import { useRouter } from 'next/router'
 export default function Budget() {
     const router = useRouter();
     return (
-        <>
-            <div className={styles.mainContainer}>
-                <div className={styles.blurContainer}></div>
-                <div className={styles.budgetContent}>
-                    <h1 style={{color: 'white'}} onClick={()=>router.push('/contato')}>Clique aqui!</h1>
-                    <p>E fale conosco para fazer seu orçamento <span style={{color: 'white'}}>agora!</span></p>
-                </div>
+        <div className={styles.mainContainer}>
+            <div className={styles.blurContainer}></div>
+            <div className={styles.budgetContent}>
+                <h1 style={{color: 'white'}} onClick={()=>router.push('/contato')}>Clique aqui!</h1>
+                <p>E fale conosco para fazer seu orçamento <span style={{color: 'white'}}>agora!</span></p>
             </div>
-        </>
+        </div>
     )
 }
