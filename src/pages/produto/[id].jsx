9fccd72fffaf0d1b9d8ea0af0843b5ab3ce1da12
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-02-18 23:57:02 -0300

    Update [id].jsx

--- src/pages/produto/[id].jsx
+++ src/pages/produto/[id].jsx
@@ -2,13 +2,9 @@ import Header from "@/components/Header/Header";
 import Product from "@/components/Product/Product";
 import Footer from "@/components/Footer/Footer";
 
-import { useRouter } from 'next/router';
 import { fetchAPI} from "@/lib/fetchAPI";
 
 export default function Trabalhos({product}){
-    const router = useRouter();
-    if(!router.isReady) return;
-    const { id } = router.query;
 
     return(
         <div>
